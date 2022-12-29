9231d403a0539fb61a2edc12064e375b1bd975c0
duongdt <doanthanhduong11031990@gmail.com>
2022-12-29 09:50:46 +0700

    add new config for k6

--- short-url/load-test/docker-k6/script.js
+++ short-url/load-test/docker-k6/script.js
@@ -3,16 +3,30 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.3/index.js";
 import { sleep } from 'k6';
 
-export const options = {
-  vus: 200,
-  duration: '10s',
+// export const options = {
+//   vus: 200,
+//   duration: '10s',
+// };
+
+let target_vus = 200
+export let options = {
+  stages: [
+      // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
+      { duration: "5s", target: target_vus },
+
+      // Stay at rest on TARGET_VUS VUs for 10s
+      { duration: "10s", target: target_vus },
+
+      // Ramp-down from TARGET_VUS to 0 VUs for 5s
+      { duration: "5s", target: 0 }
+  ]
 };
 
 export default function () {
   let randomNumber = Math.random() * 100;
   let myIp = "34.142.202.179";
   http.get('http://' + myIp + ':9898/create-url?rawUrl=https://meet.google.com/sbv-jbwj-ugg' + randomNumber);
-  sleep(1);
+  sleep(0.300);
 }
 
 // k6 run script.js --out json=test.json
